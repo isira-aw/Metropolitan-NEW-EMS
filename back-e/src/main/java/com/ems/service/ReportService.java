b3a053165e445cb2b466236d08c25abbd7ce12d7
Isira Adithya Weerasinghe <isira.aw@gmail.com>
2026-01-02 10:11:56 +0530

    ff

--- back-e/src/main/java/com/ems/service/ReportService.java
+++ back-e/src/main/java/com/ems/service/ReportService.java
@@ -8,10 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
